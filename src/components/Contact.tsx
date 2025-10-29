7059e492bc0145d54715cf36f0ef0d27756c28b0
boostconnect <academy100sales@gmail.com>
2025-10-29 10:05:48 +0000

    6 Social

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -91,6 +91,16 @@ ${formData.message}`;
                 <div className="flex items-center justify-center gap-2 text-muted-foreground">
                   <Mail className="w-5 h-5" />
                   <span>boost1connect@gmail.com</span>
+                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+                  {/* The social media icon bar */}
+                  <div className="icon-bar">
+                    <a href="#" className="facebook"><i className="fa fa-facebook"></i></a>
+                    <a href="#" className="twitter"><i className="fa fa-twitter"></i></a>
+                    <a href="#" className="google"><i className="fa fa-google"></i></a>
+                    <a href="#" className="linkedin"><i className="fa fa-linkedin"></i></a>
+                    <a href="#" className="youtube"><i className="fa fa-youtube"></i></a>
+                  </div>
                 </div>
               </div>
             </div>
